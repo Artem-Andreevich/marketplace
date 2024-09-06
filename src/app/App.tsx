ecd858a13d40b0e9df0934b806cce02fddfd6d1c
ARTEM ANDREEVICH <ops210@mail.ru>
2024-09-06 14:44:17 +0500

    install react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import AppRouter from '../routers';
 import { createPortal } from 'react-dom';
 import { Provider } from "react-redux"
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { store } from '../store';
 import { Svg } from "../layout"
 
@@ -9,6 +10,15 @@ import './globalStyles/reset.css';
 import './globalStyles/styles.css';
 import './globalStyles/media.css';
 
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
 export function App() {
 	return (
 		<>
@@ -16,9 +26,11 @@ export function App() {
 				<Svg />,
 				document.body
 			)}
-			<Provider store={store}>
-				<AppRouter />
-			</Provider>
+			<QueryClientProvider client={queryClient}>
+				<Provider store={store}>
+					<AppRouter />
+				</Provider>
+			</QueryClientProvider>
 		</>
 	);
 };
